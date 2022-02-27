459fb01ead4462b9fa53389edcdc29c6f55f990f
ajjalti <ahmed.ajjalti@gmail.com>
2022-02-27 13:31:24 +0100

    javaScripte-calculator

---
+++ app.js
@@ -0,0 +1,19 @@
+//Partie fonction de calcul :
+function sum(n,m){
+    return n+m;
+}
+function sub(n,m){
+    return n-m;
+}
+function mult(n,m){
+    return n*m;
+}
+function div(n,m){
+    return n/m;
+}
+function mod(n,m){
+    return n%m;
+}
+//partie event :
+var modulo = document.getElementsByClassName('output');
+modulo.innerText = "ici";
